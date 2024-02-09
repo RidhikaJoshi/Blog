cdb966c6dbfbba4c1dd194cb3cad4d6f27bfac4a
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-02-09 12:18:58 +0530

    Add copyright information to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-slate-400 w-full h-12 flex flex-row items-center justify-center'>
+      <div>&copy; Ridhika Joshi. All Rights Reserved.</div>
+    </div>
   )
 }
 
