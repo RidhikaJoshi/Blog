baecabe60bd6939910c9f94c6721ec075c9bee63
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-02-11 19:50:29 +0530

    Update favicon link and add responsive navigation menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,16 @@ import Logoutbtn from './Logoutbtn'
 import {Link} from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import  {useNavigate} from 'react-router-dom'
+import {useState} from 'react'
 
 function Header() {
 
+    const [showNav,setShowNav]=useState(false);
     const authStatus=useSelector((state)=>state.auth.status);
     console.log(`${authStatus} ReloadHeader`);
     const navigate=useNavigate();
+
+
 
     const navItems =[
       {
@@ -40,13 +44,14 @@ function Header() {
     ];
 
   return (
+    <>
    <div className='transparent border-b-[0.5px] border-white text-white  w-full h-[5vh] flex flex-row items-center justify-between p-8'>
         {/* This div contains logo */}
       <div className='text-[#FD356D] italic text-xl cursor-pointer'><Link to='/'>BlogSphere</Link></div>
 
     {/* This div contains list of navigation options */}
       
-        <ul className='md:flex md:flex-row md:gap-6 md:text-lg md:items-center'>
+        <ul className='md:flex md:flex-row md:gap-6 md:text-lg md:items-center hidden'>
           {navItems.map((item, index) => {
             if (item.active)
               return (
@@ -64,11 +69,36 @@ function Header() {
 
 
       {/* This div is for the hamburger menu */}
-      <div className='md:hidden flex items-center'>
-          <img className='h-10' src={hamburger}/>
-      </div>
+        <div className='md:hidden flex items-center'>
+          
+          <img className='h-10' src={hamburger} onClick={() => setShowNav(!showNav)}/>
+          
+
+        </div>
     
    </div>
+   {showNav && (
+            <div className='border-[1px] rounded border-white z-10 min-h-52 min-w-52 float-right flex items-center justify-center'>
+            <ul className='flex flex-col gap-4 items-center justify-center text-white'>
+              {navItems.map((item, index) => {
+                if (item.active) {
+                  return (
+                    <li
+                      className={`cursor-pointer ${item.name === "Login" || item.name === "SignUp" ? 'bg-[#FD356D] px-4 py-2 rounded-full' : 'hover:text-[#FD356D]'} `}
+                      onClick={() => navigate(item.slug)}
+                      key={index}
+                    >
+                      {item.name}
+                    </li>
+                  );
+                }
+              })}
+               {authStatus && (<li><Logoutbtn/></li>)}
+            </ul>
+            </div>
+          )}
+
+   </>
   )
 }
 
