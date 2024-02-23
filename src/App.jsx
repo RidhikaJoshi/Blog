981848031b1203414e747ac8fd109e7f606ec26a
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-02-23 15:39:08 +0530

    Fix loading animation in App component

--- src/App.jsx
+++ src/App.jsx
@@ -26,11 +26,13 @@ function App() {
 
     if(loading==true)
     {
-      <div class="flex flex-row gap-2">
+       return (
+        <div class="flex flex-row gap-2">
             <div class="w-4 h-4 rounded-full bg-customPink animate-bounce"></div>
             <div class="w-4 h-4 rounded-full bg-customPink animate-bounce [animation-delay:-.1s]"></div>
             <div class="w-4 h-4 rounded-full bg-customPink animate-bounce [animation-delay:-.2s]"></div>
         </div>
+    )
     }
     else
     {
