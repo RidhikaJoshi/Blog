215ede7878b5f3313ab7af569063a9128bddc8af
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-02-25 16:20:01 +0530

    Add Google sign-up functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,6 +7,7 @@ import { login } from '../store/authSlice.js'
 import {Link} from 'react-router-dom'
 import { ToastContainer,toast, Bounce } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { FcGoogle } from "react-icons/fc";
 
 
 function SignUp() {
@@ -17,6 +18,15 @@ function SignUp() {
   const dispatch = useDispatch();
   const [SignUp, setSignUp] = useState('Create Account');
 
+    const handleGoogleSignUp = async () => {
+        authService.loginWithGoogle().then((user) => {
+          if (user) {
+            dispatch(login({ userData: user }));
+            navigate('/');
+            toast.success('Loggin Successfull!');
+          }
+        });
+      };
 
   const SignUpHandler = async (e) => {
     e.preventDefault();
@@ -63,7 +73,7 @@ function SignUp() {
   }
 
   return (
-    <div className='w-full min-h-[85vh] flex items-center justify-center'>
+    <div className='w-full min-h-[100vh] flex items-center justify-center'>
         <div className=' w-[80%]  flex items-center justify-center text-xl font-medium'>
             <div className='flex  flex-col justify-evenly shadow-sm shadow-white p-6 rounded-xl gap-4'>
           <label className='text-[#FD356D]'>Name: </label>
@@ -73,6 +83,11 @@ function SignUp() {
           <label className='text-[#FD356D]'>Password: </label>
           <input type="password" name="password" value={password} className='h-10 italic outline-none p-4' placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} required />
           <button className='text-white italic bg-[#FD356D] px-4 py-2 rounded-full ' type='submit' onClick={SignUpHandler}>{SignUp}</button>
+
+           <button className="flex justify-center items-center gap-2 border-2 border-slate-600 px-5 py-2 rounded-lg"  onClick={ handleGoogleSignUp  }>
+                    <FcGoogle size={30} />
+                    <span className='text-white italic'>SignUp with Google</span>
+                </button>
           <p className='text-white'>Already have an acoount? <Link to='/login' className='text-[#FD356D]'>Login</Link></p>
           <ToastContainer 
                 style={{
