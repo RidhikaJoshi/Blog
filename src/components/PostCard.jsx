61bc18530e2faea455147cb94a098a63e454a1f1
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-03-02 20:34:56 +0530

    Update PostCard component styling

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -25,7 +25,7 @@ function PostCard({$id,title,featuredImage,author,Likes,comments}) {
     }, [featuredImage]);
      return (
       <Link to={`/blogs/${$id}`}>
-          <div className='w-80 min-h-96 p-3 shadow-sm shadow-white rounded-xl text-white flex flex-col justify-between items-center gap-4'>
+          <div className='w-80 h-[30rem] p-3 shadow-sm shadow-white rounded-xl text-white flex flex-col justify-between items-center gap-4'>
                 {imageURL && <img src={imageURL} alt={title} className='h-[200px] w-full'/>}
                      <h2 className='text-xl text-center font-bold'>{title}</h2>
                      <p className='text-sm text-center '>Author: {author}</p>
