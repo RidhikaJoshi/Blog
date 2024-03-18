6ed9d49d143bf175a0fc57611c847f120762dbc0
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-03-18 21:40:03 +0530

    Update login page with test user login functionality

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { login } from '../store/authSlice.js'
 import { Link } from 'react-router-dom'
-import { ToastContainer, toast, Bounce } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import { FcGoogle } from "react-icons/fc";
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -13,6 +13,7 @@ function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [Login, setLogin] = useState('Login');
+  const [loginTest, setLoginTest] = useState('Login as Test User');
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -62,6 +63,24 @@ function LoginPage() {
       }
     })
   }
+  const loginTestHandler = async () => {
+    setLoginTest ('Logging in...');
+    const testEmail = "test@test.com";
+    const testPassword = "12345678";
+    setEmail(testEmail);
+    setPassword(testPassword);
+    const user = await authService.login(testEmail, testPassword);
+    //console.log(user);
+    if (!user) {
+      toast.error('Internal Server Error');
+      setEmail('');
+      setPassword('');
+      setLogin('Login');
+    } else {
+      dispatch(login({ userData: user }));
+      navigate('/');
+    }
+  }
 
   return (
     <div className='w-full min-h-[85vh] flex items-center justify-center'>
@@ -72,6 +91,7 @@ function LoginPage() {
           <label className='text-[#FD356D]'>Password: </label>
           <input type="password" name="password" value={password} className='h-10 italic outline-none p-4' placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} required />
           <button className='text-white italic bg-[#FD356D] px-4 py-2 rounded-full ' type='submit' onClick={loginHandler}>{Login}</button>
+          <button className='text-white italic bg-[#FD356D] px-4 py-2 rounded-full ' type='submit' onClick={loginTestHandler}>{loginTest}</button>
           <button className="flex justify-center items-center gap-2 border-2 border-slate-600 px-5 py-2 rounded-lg" onClick={handleGoogleLogin}>
             <FcGoogle size={30} />
             <span className='text-white italic'>Login with Google</span>
